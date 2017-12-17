d3a96146812d0b5f17ab41584103e72f00d91c38
shaneoverby <soverby@core-techs.net>
2017-12-16 22:02:39 -0500

    initial

---
+++ RewardedNation/Controllers/UploadsController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RewardedNation.Controllers
+{
+    public class UploadsController : Controller
+    {
+        // GET: Uploads
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(HttpPostedFileBase zip)
+        {
+            var uploads = Server.MapPath("~/uploadedFiles");
+            using(ZipArchive archive = new ZipArchive(zip.InputStream, ZipArchiveMode.Read))
+            {
+                foreach(ZipArchiveEntry entry in archive.Entries)
+                {
+                    var ext = Path.GetExtension(entry.FullName);
+                    if (!string.IsNullOrEmpty(ext))
+                        entry.ExtractToFile(Path.Combine(uploads, entry.FullName));
+                    else
+                        Directory.CreateDirectory(Path.Combine(uploads, entry.FullName));
+                }
+            }
+            ViewBag.FileNames = Directory.GetFiles(uploads, "*.*", SearchOption.AllDirectories).Select(Path.GetFileName).ToArray();
+            return View();
+        }
+    }
+}
---
+++ RewardedNation/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(RewardedNation.Startup))]
+namespace RewardedNation
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
